7f01ce33701c60cfc54cbcbb634e8469214b3a6d
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-04-10 01:35:19 +0900

    ページ遷移するモーションを変更

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,23 @@ export default function Layout() {
       screenOptions={{
         headerTintColor: "#000000",
         headerStyle: { backgroundColor: "yellowgreen" },
+        animation: "none",
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
 
-      <Stack.Screen name="home/index" options={{ headerTitle: "客室状況" }} />
-      <Stack.Screen name="task/index" options={{ headerTitle: "タスク" }} />
+      <Stack.Screen
+        name="home/index"
+        options={{ headerTitle: "客室状況", headerBackVisible: false }}
+      />
+      <Stack.Screen
+        name="task/index"
+        options={{ headerTitle: "タスク", headerBackVisible: false }}
+      />
+      <Stack.Screen
+        name="task/completed"
+        options={{ headerTitle: "完了したタスク", animation: "default" }}
+      />
     </Stack>
   );
 }
