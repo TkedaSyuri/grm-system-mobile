86547511d59f03a6fb2b50e1087568be637711e7
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-04-10 01:35:41 +0900

    completed.tsxページを追加

---
+++ app/task/completed.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet,ScrollView } from "react-native";
+import { useAtomValue } from "jotai";
+import { tasksAtom } from "../../src/store";
+import CompletedTask from "../../src/components/CompletedTask";
+
+const CompletedTaskList = () => {
+  const TasksData = useAtomValue(tasksAtom);
+
+  return (
+      <ScrollView
+        style={styles.scrollArea}
+        contentContainerStyle={{ paddingVertical: 50 }}
+      >
+        {TasksData.map((task) => (
+          <CompletedTask
+            key={task.id}
+            id={task.id}
+            task={task.task}
+            isCompleted={task.isCompleted}
+          />
+        ))}
+      </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignContent: "center",
+
+  },
+  scrollArea: {
+    flex: 1,
+  },
+  menuBarContainer: {
+    flex: 0.1,
+    backgroundColor: "yellowgreen",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default CompletedTaskList;
