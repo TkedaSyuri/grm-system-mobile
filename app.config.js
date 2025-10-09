198d30b39bf92fedc9c14be4fd8c57f7d4f537f8
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-10-09 15:48:43 +0900

    app.config.jsファイルを追加

--- app.config.js
+++ app.config.js
@@ -2,10 +2,20 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "my-app",
-    slug: "my-app",
+    name: "grm-system-mobile",
+    slug: "grm-system-mobile",
+    owner: "syuri2000",
     extra: {
       API_BASEURL: process.env.API_BASEURL,
+      eas: {
+        projectId: "08fc4b7e",
+      },
+    },
+    updates: {
+      url: "https://u.expo.dev/08fc4b7e",
+    },
+    runtimeVersion: {
+      policy: "appVersion",
     },
   },
 };
