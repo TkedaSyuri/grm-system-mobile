c39d0dce7ac3733cc69e2e205df6e33cfed5a849
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-10-22 16:22:09 +0900

    命名の綴りミスを修正

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,7 @@ import type { Chats, Floors, Tasks } from "../types";
 export const floorsAtom = atom<Floors[]>([])
 export const tasksAtom = atom<Tasks[]>([])
 export const chatsAtom = atom<Chats[]>([])
-export const floorNuberAtom = atom("2")
+export const floorNumberAtom = atom("2")
 export const selectedRoomNumbersAtom = atom<string>("");
 export const selectedRoomIdAtom = atom<number>(0)
 export const isModalAtom = atom<boolean>(false)
